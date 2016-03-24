c78e89a2166c69b4b80dbadd2b735d1b7e4071bb
Marcelo Olivas <mfolivas@gmail.com>
2016-03-24 14:57:28 -0400

    Initial example.

---
+++ src/main/java/com/mfolivas/examples/kinesis/KinesisExample.java
@@ -0,0 +1,10 @@
+package com.mfolivas.examples.kinesis;
+
+/**
+ * Kinesis example.
+ */
+public class KinesisExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
